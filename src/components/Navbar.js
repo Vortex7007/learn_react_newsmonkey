874cda46cee0776276660f56989400b22bf3db47
Vortex7007 <125956254+Vortex7007@users.noreply.github.com>
2024-08-16 15:17:46 +0530

    navbar to function based component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
 import {Link} from "react-router-dom";
 
-export class Navbar extends Component {
-  render() {
+const Navbar = (props)=>{
     return (
       <div>
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
@@ -44,6 +43,5 @@ export class Navbar extends Component {
       </div>
     )
   }
-}
 
 export default Navbar
