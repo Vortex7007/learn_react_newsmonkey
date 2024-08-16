ce59aceab838c8998e1a3a029c2c0e1af87ae370
Vortex7007 <125956254+Vortex7007@users.noreply.github.com>
2024-08-16 15:25:51 +0530

    newsitem and spinner converted to funtion based component

--- src/components/News.js
+++ src/components/News.js
@@ -35,7 +35,7 @@ export class News extends Component {
   async componentDidMount(props) {
     this.props.setProgress(30)
     this.setState({ page: this.state.page + 1 });
-    // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apikey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apikey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true })
     let data = await fetch(url);
     this.props.setProgress(50)
@@ -51,7 +51,7 @@ export class News extends Component {
 
    fetchMoreData = async()=>{
     this.setState({page : this.state.page +1})
-    // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apikey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apikey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
     let parsedData = await data.json()
     this.setState({
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,29 +1,27 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class NewsItem extends Component {
-  render() {
-    let { title, description, imageUrl, newsUrl, author, date,source } = this.props;
-    return (
-      <div className="card my-2">
-        <div style={{
-          display:"flex",
-          justifyContent: "flex-end",
-          position: "absolute",
-          right: "0"
-        }}>
-        <span class="badge rounded-pill bg-danger">{source}
-        </span>
-        </div>
-        <img src={imageUrl} className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">{title}...</h5>
-          <p className="card-text">{description}...</p>
-          <p className='card-text'><small className='text-muted'>By {!author ? "Unknown" : author} on {new Date(date).toGMTString()}</small></p>
-          <a rel="noreferrer" href={newsUrl} target='_blank' className="btn btn-sm btn-dark">Details</a>
-        </div>
-      </div>
-    )
+const NewsItem = (props)=>{
+    let { title, description, imageUrl, newsUrl, author, date, source } = props;
+return (
+  <div className="card my-2">
+    <div style={{
+      display: "flex",
+      justifyContent: "flex-end",
+      position: "absolute",
+      right: "0"
+    }}>
+      <span class="badge rounded-pill bg-danger">{source}
+      </span>
+    </div>
+    <img src={imageUrl} className="card-img-top" alt="..." />
+    <div className="card-body">
+      <h5 className="card-title">{title}...</h5>
+      <p className="card-text">{description}...</p>
+      <p className='card-text'><small className='text-muted'>By {!author ? "Unknown" : author} on {new Date(date).toGMTString()}</small></p>
+      <a rel="noreferrer" href={newsUrl} target='_blank' className="btn btn-sm btn-dark">Details</a>
+    </div>
+  </div>
+)
   }
-}
 
 export default NewsItem
